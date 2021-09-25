91e2bae6228b4bec33dc12ba4420c81fe3ae78e6
William Jessop <williamjessop12@outlook.com>
2021-09-25 10:51:24 -0600

    put some more work into Animation loader and added its own file

--- src/Particle.hpp
+++ src/Particle.hpp
@@ -1,90 +1,8 @@
 #pragma once
 
-#include <vector>
-#include <fstream>
+#include "Animation.hpp"
 
 using namespace std;
-
-class AnimationFrame{
-	SDL_Rect frame;
-	int millis;
-	public:
-	AnimationFrame(int newMillis, int newX, int newY, int newW, int newH) {
-		millis=newMillis;
-		frame.x = newX;
-		frame.y = newY;
-		frame.w = newW;
-		frame.h = newH;
-	}
-	void read(MediaManager *media,ifstream &in) {
-		
-	}
-	int getMillis() { return millis; }
-	SDL_Rect *getFrame() { return &frame; }
-};
-
-class Animation{
-	SDL_Texture *spriteSheet;
-	vector<AnimationFrame*> frames;
-
-	int totalTime;
-	int currentTime;
-
-	string sheetName;
-
-	public:
-	Animation() { 
-	  totalTime=0;
-	  currentTime=0;
-	}
-
-	void read(MediaManager *media,string file) {
-		int max;
-		ifstream in;
-
-		cout << file << endl;
-
-		in.open(file);
-
-		in >> max >> sheetName;
-		spriteSheet=media->read("media/"+sheetName+".bmp");
-		
-		int millis,x,y,w,h;
-		for (int i=0;i<max;i++) {
-			in >> millis >> x >> y >> w >> h;
-			AnimationFrame *af=new AnimationFrame(millis, x, y, w, h);
-			
-			frames.push_back(af);
-			totalTime+=af->getMillis(); 
-		}
-		in.close();
-	}
-
-	void update(double dt) {
-		currentTime+=(int)(dt*1000.0);
-		//cout << "Total Time " << totalTime << endl;
-		currentTime%=totalTime;
-	}
-
-	SDL_Rect *getFrame() {
-		int checkTime=0;
-		int t=0;
-		for (t=0;t<frames.size();t++) {
-			if (checkTime+frames[t]->getMillis()>currentTime) 
-			  break;
-			checkTime+=frames[t]->getMillis();
-		}
-		if (t==frames.size()) t=0;
-		return frames[t]->getFrame();
-	}
-
-	SDL_Texture *getTexture(){ return spriteSheet; }
-
-	~Animation() {
-		for (auto f:frames) 
-		  delete f;
-	}
-};
 
 class Particle {
 	SDL_Renderer *ren;
@@ -123,6 +41,8 @@ class Particle {
 		maxx=newMaxX;
 		maxy=newMaxY;
 	}
+
+	void setAnimation(Animation *newA){ a=newA; }
 	
 	void update(double dt) {
 		if (maxx!=minx) {
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "MediaManager.hpp"
 #include "Game.hpp"
 #include "Particle.hpp"
+#include "Animation.hpp"
 
 #define PI 3.14159265
 
@@ -15,18 +16,19 @@ using namespace std;
 
 class MyGame:public Game {
 	vector <Particle*> particles;
-    Animation a;
-
+    Animation brightSound, midSound, dimSound;
 
 	public:
 	MyGame(int w=640, int h=480):Game("Echos", w, h) {
-		a.read(media, "media/sound1.txt");
+		brightSound.read(media, "media/sound1.txt");
+		midSound.read(media, "media/sound2.txt");
+		dimSound.read(media, "media/sound3.txt");
 
         for(int i=0; i < 360; i++){
             double vy = sin (i*PI/180) * 100;
 			double vx = cos (i*PI/180) * 100;
 
-            particles.push_back(new Particle(ren, &a, w/2, h/2, vx, vy, 0.0, 0.0, 0.9));
+            particles.push_back(new Particle(ren, &brightSound, w/2, h/2, vx, vy, 0.0, 0.0, 0.9));
 		 	particles[i]->setBound(0,0,w-8,h-8);
         }
 	}
