f2f1d0e8602e502b91bf3c85ad22225edccf84d7
Karson-Fye <69867634+Karson-Fye@users.noreply.github.com>
2021-11-02 17:37:45 -0600

    Map and Object Headers - Floors made

---
+++ src/Map.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <SDL_mixer.h>
+#include <SDL.h>
+
+#include "MediaManager.hpp"
+#include "Particle.hpp"
+#include "Animation.hpp"
+#include "Wave.hpp"
+#include "Player.hpp"
+#include "Entity.hpp"
+#include "Object.hpp"
+#include "Config.hpp"
+
+class Map {
+  
+};
---
+++ src/Object.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <map>
+#include <SDL_mixer.h>
+#include <SDL.h>
+
+#include "Particle.hpp"
+#include "Animation.hpp"
+#include "MediaManager.hpp"
+#include "Config.hpp"
+#include "Wave.hpp"
+
+using namespace std;
+
+class Object:public Particle{
+  Config *cfg;
+  SDL_Renderer *ren;
+  MediaManager *media;
+
+  map<string,Animation *> animations;
+  map<string, Mix_Chunk *> sounds;
+
+  double baseSpeed;
+  
+protected:
+  Animation *a;
+  SDL_Rect dest;
+  Waves *waves;
+
+public:
+  Object(MediaManager *newMedia, SDL_Renderer *newRen, Waves *newWaves, Config *newCfg,
+  double newx=0.0, double newy=0.0,
+  double newv=0.0, int newtheta=0,
+  double newax=0.0, double neway=0.0,
+  double newdamp=0.0):Particle(newx, newy, newv, newtheta, newax, neway, newdamp) {
+
+    ren=newRen;
+    media=newMedia;
+    cfg=newCfg;
+    waves=newWaves;
+    
+    dest.w = stoi((*cfg)["width"]);
+    dest.h = stoi((*cfg)["height"]);
+
+    vector<string> newAnimations = cfg->getMany("animations");
+		for(auto anim: newAnimations){
+			animations[anim] = new Animation();
+			animations[anim]->readAnimation(media, anim);
+		}
+
+		a=animations[(*cfg)["defaultAnimation"]];
+		
+		vector<string> newSounds = cfg->getMany("sounds");
+		for(auto sound: newSounds){
+			sounds[sound] = new Mix_Chunk();
+			sounds[sound] = media->readSound(sound);
+    }
+  }
+
+  SDL_Rect *getDest() { return &dest; }
+
+  void setAnimation(Animation *newA) { a=newA; }
+
+  void update(double dt) {
+    SDL_RenderCopy(ren, a->getTexture(), a->getFrame(), &dest);
+  }
+
+  ~Object() {
+    for (auto a:animations) delete a.second;
+    for (auto s:sounds) delete s.second;
+  }
+};
